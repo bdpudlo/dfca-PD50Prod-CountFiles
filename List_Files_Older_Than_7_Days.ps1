b28d0bb6a026e3bf96ca9ba714181198ed23a93b
Brian Abrams <Brian.Abrams@Doosan.com>
2019-02-08 15:27:19 -0500

    Init Check in

---
+++ List_Files_Older_Than_7_Days.ps1
@@ -0,0 +1,13 @@
+﻿#mkdir C:\PD50prod_reports
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x750A -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count > C:\PD50prod_reports\x750A.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x750B -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count > C:\PD50prod_reports\x750B.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x751A -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count> C:\PD50prod_reports\x751A.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x751B -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count> C:\PD50prod_reports\x751B.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x781 -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count> C:\PD50prod_reports\x781.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x782 -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count> C:\PD50prod_reports\x782.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x783 -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count> C:\PD50prod_reports\x783.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x784 -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)}| Measure-Object).count > C:\PD50prod_reports\x784.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x784B -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count> C:\PD50prod_reports\x784B.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x786 -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count> C:\PD50prod_reports\x786.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x787 -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)}| Measure-Object).count > C:\PD50prod_reports\x787.txt
+(Get-ChildItem -Path \\swc-us-pd50prod\pd50prod\x831 -Recurse | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | Measure-Object).count> C:\PD50prod_reports\x831.txt
